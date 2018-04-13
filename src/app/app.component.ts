736b0ff8841a1aa0f24a0547fdbdd5697ef12fff
hphuoclam <hphuoclam92@gmail.com>
2018-04-13 20:35:14 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  ranges  = Array.from(Array(500).keys());
+  checkArr = [];
+  results = [];
+  changeCheckbox(item) {
+  	if(this.checkArr[item]){
+  		this.results.push(item);
+  	}else{
+  		this.results.splice(this.results.indexOf(item), 1);
+  	}
+  }
 }
